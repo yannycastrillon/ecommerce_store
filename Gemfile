ca8de692b5214d4f78d35072730612eb8f584f79
Yanny Castrillon <yannyandres117@gmail.com>
2017-07-27 16:37:17 -0700

    Generate devise views with the purpose of styling and customize the actual forms. Create a config.scss to be able to create variables for styling. Apply the same format to all the form fields and form-groups of templates. Add bootstrap and bootstrap-sprockets to the manifest to be able to use the bootstrap layout. Add before action 'after_initialize' to the User model to set the default role.

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ git_source(:github) do |repo_name|
 end
 
 gem 'devise'
-
+gem 'bootstrap-sass', '~> 3.3.6'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.4'
 # Use postgresql as the database for Active Record
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ApplicationRecord
   # ActiveRecord method enum, maps each symbol to an index in the db.
   enum role: [:customer, :admin]
+  # This is a callback conditional
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :customer
+  end
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def set_default_role
-    self.role ||= :customer
-  end
 end
